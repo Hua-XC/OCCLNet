052df0cff52036aa321cac164104989a2060b160
华雪成 <59186594+Hua-XC@users.noreply.github.com>
2025-04-01 11:05:31 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -107,6 +107,7 @@ start_epoch = 0
 print('==> Loading data..')
 # Data loading code
 # normalize
+##
 normalize = transforms.Normalize(
     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
